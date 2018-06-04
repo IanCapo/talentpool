fd45f7686ae374ef6b892901a95f80a8f2aabc5b
Fabian Pasin <fabian.pasin@gmail.com>
2018-06-04 11:51:42 +0200

    Add UserItems

---
+++ src/reducers/initialState.js
@@ -0,0 +1,34 @@
+export default {
+  users: [
+    {
+      name: 'Fabian',
+      location: 'hamburg',
+      status: ' not available',
+      skills: ['html', 'css', 'javascript'],
+    },
+    {
+      name: 'Max',
+      location: 'hamburg',
+      status: 'available',
+      skills: ['html', 'css', 'React'],
+    },
+    {
+      name: 'Christan',
+      location: 'hamburg',
+      status: 'available',
+      skills: ['html', 'css', 'javascript'],
+    },
+    {
+      name: 'Hans',
+      location: 'hamburg',
+      status: 'not available',
+      skills: ['html', 'javascript', 'Sketch'],
+    },
+    {
+      name: 'Janna',
+      location: 'hamburg',
+      status: 'available',
+      skills: ['html', 'css', 'javascript'],
+    },
+  ],
+}
