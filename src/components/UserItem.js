113bd45f13fc5ead2d7d23316bc07b0b2cfdad9c
Fabian Pasin <fabian.pasin@gmail.com>
2018-06-05 10:42:49 +0200

    Ready for code review 2(userItem)

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
+import { css } from 'emotion'
 
 export default class UserItem extends Component {
   render() {
     return (
       <div>
         {this.props.users.map(user => (
-          <div>
-            <h3>
-              {user.name} | {user.location}
-            </h3>
-            <p>{user.status}</p>
-            <p>
-              {user.skills[0]} {user.skills[1]} {user.skills[2]}
-            </p>
+          <section
+            className={css`
+              display: flex;
+              flex-direction: row;
+            `}
+          >
             <div>
-              <img src={user.photo} alt="" />
+              <img src={user.photo} alt="" width="100px" />
             </div>
-          </div>
+            <div
+              className={css`
+                margin-left: 5px;
+              `}
+            >
+              <h3>
+                {user.name} | {user.location}
+              </h3>
+              <p>{user.status}</p>
+              <p>
+                {user.skills[0]} {user.skills[1]} {user.skills[2]}
+              </p>
+            </div>
+          </section>
         ))}
       </div>
     )
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,4 +1,4 @@
-import testphoto from '../images/foto.png'
+import testphoto from '../images/foto.jpg'
 
 export default {
   users: [
@@ -14,24 +14,28 @@ export default {
       location: 'hamburg',
       status: 'available',
       skills: ['html', 'css', 'React'],
+      photo: testphoto,
     },
     {
       name: 'Christan',
       location: 'hamburg',
       status: 'available',
       skills: ['html', 'css', 'javascript'],
+      photo: testphoto,
     },
     {
       name: 'Hans',
       location: 'hamburg',
       status: 'not available',
       skills: ['html', 'javascript', 'Sketch'],
+      photo: testphoto,
     },
     {
       name: 'Janna',
       location: 'hamburg',
       status: 'available',
       skills: ['html', 'css', 'javascript'],
+      photo: testphoto,
     },
   ],
   filter: [{ option: 'html' }, { option: 'css' }, { option: 'javascript' }],
